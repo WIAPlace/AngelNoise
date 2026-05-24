009b60919f05ebab6a17a839c90e73bfcd1373e6
WesInAPlace <143664522+WIAPlace@users.noreply.github.com>
2026-05-24 16:37:12 -0500

    Slightly less basic movement

--- Assets/Code/Controls/ControlStates/ControlState_Movement.cs
+++ Assets/Code/Controls/ControlStates/ControlState_Movement.cs
@@ -1,17 +1,28 @@
+//using System.Numerics;
 using UnityEngine;
 
 // Child of ControlState_abs. This is the controller for movement;
 public class ControlState_Movement : ControlState_Abs
 {
     [SerializeField] private float moveSpeed;
-    //[SerializeField] private GameObject playerBody;
+    [SerializeField] private float smoothMovement;
+
     public Vector2 moveDir;
-    //[SerializeField] private CharacterController controller;
+    private Vector3 directionalMovement;
+    private Vector3 targetVelocity;
+    
+    //private float velocity;
+
+    private Vector3 currentVelocity;
+    private Vector3 smoothDampVelocity;
+    
 
     /////////////////////////////////// DO ENTER
     public override void DoEnter() // When the state begins.
     {
         input.MoveEvent += HandleMove;
+        currentVelocity = Vector3.zero;
+        smoothDampVelocity = Vector3.zero;
     }
 
     /////////////////////////////////// DO EXIT
@@ -38,14 +49,29 @@ public class ControlState_Movement : ControlState_Abs
         { // If the player has no controls down, so isnt touching any directional input, dont move
             //currentStepTime=stepTime; // reset both of these because we have stoped walking
             //newStepTime = 0;
+
+            if(currentVelocity.magnitude > .1) // slow down till stopped
+            {     
+                // move to stop in previous direction.
+                currentVelocity = Vector3.SmoothDamp(currentVelocity, Vector3.zero, ref smoothDampVelocity, smoothMovement);        
+                controller.Move(currentVelocity *Time.deltaTime);  
+            }
+            else if(currentVelocity.magnitude != 0)
+            {
+                currentVelocity = Vector3.zero; // stop moving
+            }
+
             return;
         }
         //Debug.Log("Moving");
-        Vector3 directionalMovement = playerBody.transform.forward * moveDir.y + playerBody.transform.right * moveDir.x;
+        directionalMovement = playerBody.transform.forward * moveDir.y + playerBody.transform.right * moveDir.x;
+        targetVelocity = directionalMovement * moveSpeed;
+        
+        // smooth damp for a bit of slide.
+        currentVelocity = Vector3.SmoothDamp(currentVelocity, targetVelocity, ref smoothDampVelocity, smoothMovement);
 
-        // move in the desired direction.
-        Vector3 movement = directionalMovement * moveSpeed * Time.deltaTime;
-        controller.Move(movement);
+        
+        controller.Move(currentVelocity *Time.deltaTime);
 
         /*
         // audio for footsteps
